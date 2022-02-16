57439675cca1fbcf56232e40a00ae87dda2867ed
Saúl ito Menchén Chacón <saul.menchen.chacon@gmail.com>
2022-02-16 20:42:05 +0100

    funcioneslambda

--- funciones1nivel.py
+++ funciones1nivel.py
@@ -1,16 +1,23 @@
 def normal(i):
     return i
 
-def cuadrado (y):
-    return y * y
+
+normal = lambda x: x
+
+def cuadrado (x):
+    return x*x
+
+def cubo (x):
+    return x**3
 
 def sumaTodos (limitTo, f):
     resultado = 0 
     for i in range(limitTo+1):
-        resultado += f(1)
+        resultado += f(i)
         
         
     return resultado 
 
-print(sumaTodos(100, normal))
-print(sumaTodos(3, cuadrado))
+if __name__ == '__main__':
+    print(sumaTodos(100, normal))
+    print(sumaTodos(3, cuadrado))
